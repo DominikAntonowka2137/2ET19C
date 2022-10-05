abb55a4a4af115db6b38f19b0d9edf3329734993
10DM <114576937+DominikAntonowka2137@users.noreply.github.com>
2022-10-05 11:00:08 +0200

    Dodano KartaPracy2.cs

---
+++ KartaPracy2.cs
@@ -0,0 +1,99 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+                        // 1
+                        int a = int.Parse(Console.ReadLine());
+                        if (a%3==0)
+                        {
+                            Console.WriteLine("Tak");
+                        }
+                        else
+                        {
+                            Console.WriteLine("Nie");
+                        }
+            */
+
+            /*
+                        //2
+                        int a = int.Parse(Console.ReadLine());
+                        if (a>=100 && a<1000 && a % 17 == 0)
+                        {
+                            Console.WriteLine("Jest");
+                        }
+                        else
+                        {
+                            Console.WriteLine(("Nie jest");
+                        }
+            */
+
+            /*
+                        //3
+                        int a = int.Parse(Console.ReadLine());
+                        if (a>=18)
+                        {
+                            Console.WriteLine("Tak");
+                        }
+                        else
+                        {
+                            Console.WriteLine("Nie");
+                        }
+            */
+
+            /*
+                        //4
+                        int a = int.Parse(Console.ReadLine());
+                        const int LIMIT = 20;
+                        if (a<=LIMIT)
+                        {
+                            Console.WriteLine("Tak");
+                        }
+                        else
+                        {
+                            Console.WriteLine("Nie");
+                        }
+            */
+
+
+            /*
+                        //5
+                        int a = int.Parse(Console.ReadLine());
+                        int b = int.Parse(Console.ReadLine());
+                        int c = int.Parse(Console.ReadLine());
+                        if ((a<c) && (b>c) || (b<c) && (a>c))
+                        {
+                            Console.WriteLine("Tak");
+                        }
+                        else
+                        {
+                            Console.WriteLine("Nie");
+                        }
+            */
+
+/*
+            //6
+            int a = int.Parse(Console.ReadLine());
+            int p = int.Parse(Console.ReadLine());
+            if ((Math.Pow(a, p) - a) % p == 0)
+            {
+                Console.WriteLine("Tak");
+            }
+            else
+            {
+                Console.WriteLine("Nie");
+            }
+*/
+
+
+        }
+    }
+}
