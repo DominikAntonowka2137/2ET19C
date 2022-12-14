70ecda20b833ecd8c0f63b756a530bd500f52d3a
10DM <114576937+DominikAntonowka2137@users.noreply.github.com>
2022-12-14 12:12:28 +0100

    zaaktualizowano kartaobjektow2.cs

--- kartaobjektow2.cs
+++ kartaobjektow2.cs
@@ -6,18 +6,30 @@ using System.Threading.Tasks;
 
 namespace ConsoleApp5
 {
-        class Koszyk
+    class Koszyk
     {
         private int id;
         private Produkt produkt;
         private Uzytkownik uzytkownik;
-        public string getId()
+        public int getId()
         {
             return this.id;
         }
-       public void operation()
+        public void setId(int aid)
+        {
+            this.id = aid;
+        }
+        public void operation()
         {
             Console.WriteLine("Operacja");
+        }
+        public void setUzytkownik(Uzytkownik auzytkownik)
+        {
+            this.uzytkownik = auzytkownik;
+        }
+        public Koszyk(Produkt aprodukt)
+        {
+            this.produkt=aprodukt;
         }
     }
     class Uzytkownik
@@ -29,6 +41,10 @@ namespace ConsoleApp5
         {
             Console.WriteLine("loguje");
         }
+        public Uzytkownik(Profil aprofil)
+        {
+            this.profil = aprofil;   
+        }
     }
     class Profil
     {
@@ -39,7 +55,7 @@ namespace ConsoleApp5
         {
             return this.imie;
         }
-       public string getNazwisko()
+        public string getNazwisko()
         {
             return this.nazwisko;
         }
@@ -49,22 +65,24 @@ namespace ConsoleApp5
         private string nazwa;
         private double cena;
         private string opis;
-        public void getNazwa()
+        public string getNazwa()
         {
             return this.nazwa;
         }
-        public void getCena()
+        public double getCena()
         {
             return this.cena;
         }
-        public void getOpis()
+        public string getOpis()
         {
             return this.opis;
         }
 
-        public void produkt(nazwa, cena, opis)
+        public Produkt(string anazwa,double acena,string aopis)
         {
-
+            this.nazwa = anazwa;
+            this.cena = acena;
+            this.opis = aopis;
         }
     }
     internal class Program
@@ -72,8 +90,10 @@ namespace ConsoleApp5
         static void Main(string[] args)
         {
             Profil p = new Profil();
-            Uzytkownik u = new Uzytkownik();
-            Produkt r = new Produkt();
+            Uzytkownik u = new Uzytkownik(p);
+            Produkt prod = new Produkt("nazwa", 312.99, "opis");
+            Koszyk k = new Koszyk(prod);
+            k.setUzytkownik(u);
         }
     }
 }
